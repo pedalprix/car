a28fe671030a6b6ab208cddcb2baa164965bcd46
Steve <wisefuture0@gmail.com>
2016-09-11 15:21:25 +0930

    little change to see if it works

--- runme.sh
+++ runme.sh
@@ -2,8 +2,9 @@
 #
 # This bash script is called from .bashrc after the car performs "git pull origin master from "
 
-cd ~/car
-
 # Start the python scripts
-./python Car-TxUDP-GPS.py &
-./python Car-TxUDP-RFID.py &
+#./python Car-TxUDP-GPS.py &
+#./python Car-TxUDP-RFID.py &
+
+ls
+echo "Hello Steve!"
