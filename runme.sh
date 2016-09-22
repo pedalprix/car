42e355b8dad6dac1d2e2e89bedd8f1655e375ee0
Steve <wisefuture0@gmail.com>
2016-09-22 22:48:01 +0930

    disable python scripts for testing

--- runme.sh
+++ runme.sh
@@ -6,8 +6,8 @@ echo "PedalPrix: Start runme.sh"
 
 # Start the python scripts
 echo "PedalPrix: Starting Car-TxUDP-GPS.py in background"
-python Car-TxUDP-GPS.py > GPS.log &
+# python Car-TxUDP-GPS.py > GPS.log &
 echo "PedalPrix: Starting Car-TxUDP-RFID.py in background"
-python Car-TxUDP-RFID.py >RFID.log &
+# python Car-TxUDP-RFID.py >RFID.log &
 ps
 echo "PedalPrix: Finish runme.sh"
